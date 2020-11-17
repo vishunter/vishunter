ddec72c903cfbf44d2eff6c03e349353eb4aaa65
vishunter <65365939+vishunter@users.noreply.github.com>
2020-11-17 11:33:48 +0530

    Add files via upload

---
+++ textbanner vishunter.cmd
@@ -0,0 +1,54 @@
+@echo off 
+
+cd/
+
+cd/Users\vishu\Desktop\Git
+
+cls
+
+rem personal vishunter command bash
+
+Set/a num=(%Random% %%9)+1
+
+color %num%
+
+rem color 07 
+
+title Made by vishal chauhan !!! 
+
+
+
+curl "https://devops.datenkollektiv.de/renderBannerTxt?text=vishunter&font=soft"
+
+echo.
+
+echo						 power by VISHAL_CHAUHAN ... 
+
+rem echo. && pause
+
+echo.
+
+echo.
+
+echo                ########################################  
+
+echo                # playing a movie in command promt ... #  
+
+echo                ######################################## 
+
+echo. && pause
+
+echo.
+
+
+telnet towel.blinkenlights.nl. 
+
+pause
+
+
+
+
+
+
+
+
